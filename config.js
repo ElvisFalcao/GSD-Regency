405ddda2946ae06127ba292ad2d3857af56947bb
Elvis <denyfalcao008@gmail.com>
2026-07-24 01:41:04 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// Populate these public values from the existing FluxPlanner Supabase project.
+// Leave blank to run the interface in local demo mode.
+window.PM_CONFIG = {
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  workspaceId: 'regency-shalina',
+  timezone: 'Africa/Johannesburg'
+};
+
