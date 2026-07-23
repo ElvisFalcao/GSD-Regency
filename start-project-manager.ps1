9fd74823bd6182fc1370c4f7e2cefe64fbc23a9d
Elvis <denyfalcao008@gmail.com>
2026-07-24 01:42:11 +0200

    Add start-project-manager.ps1

---
+++ start-project-manager.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Set-Location $PSScriptRoot
+if (Get-Command py -ErrorAction SilentlyContinue) {
+  py -m http.server 4173
+} elseif (Get-Command python -ErrorAction SilentlyContinue) {
+  python -m http.server 4173
+} else {
+  Write-Error 'Python is required. Install Python or run the app from another local static server.'
+}
+
